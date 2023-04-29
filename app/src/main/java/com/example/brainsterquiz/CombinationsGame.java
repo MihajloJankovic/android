dbbe4e0651fafd6816c0c94b541e421c75e73a70
Sovlja <101639981+Sovlja@users.noreply.github.com>
2023-04-29 03:59:25 +0200

    Step by step improved!

---
+++ app/src/main/java/com/example/brainsterquiz/CombinationsGame.java
@@ -0,0 +1,15 @@
+package com.example.brainsterquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CombinationsGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combinations_game);
+        getSupportActionBar().hide();
+    }
+}
